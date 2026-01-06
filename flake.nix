42159f46ad38831981799d0d783d264d8588417e
Pial Das <pdas2711@proton.me>
2026-01-05 23:27:26 -0500

    BROKEN: vcpkg is used to install build dependencies. Returned invalid triplet: x64-linux.

--- flake.nix
+++ flake.nix
@@ -10,47 +10,36 @@
 
 	outputs = { self, nixpkgs, ... }@inputs:
 	let
-		allSystems = [ "x86_64-linux" ];
-		forAllSystems = f: nixpkgs.lib.genAttrs allSystems (
-			system:
-			f {
-				pkgs = import nixpkgs { inherit system; };
-			}
-		);
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+		binName = "audioshare";
 	in {
-		packages = forAllSystems ({ pkgs }: {
-			default =
-			let
-				binName = "audioshare";
-			in
-				pkgs.stdenv.mkDerivation {
-					pname = "audioshare";
-					version = "0.3.4";
-					src = inputs.audioshare;
-					nativeBuildInputs = with pkgs; [
-						cmake
-						pkg-config
-					];
-					buildInputs = with pkgs; [
-						vcpkg
-						asio
-						protobufc
-						cxxopts
-						spdlog
-					];
-					buildPhase = ''
-						cmake --preset linux-Release
-						cmake --build --preset linux-Release
-					'';
-					installPhase = ''
-						mkdir -p $out/bin
-						cp out/install/linux-Release/bin/as-cmd $out/bin/${binName}
-					'';
-					env = {
-						VCPKG_TARGET_TRIPLET = "x64-linux";
-						VPCKG_ROOT = ".";
-					};
-				};
-		});
+		packages.${system}.default = pkgs.stdenv.mkDerivation {
+			pname = "audioshare";
+			version = "0.3.4";
+			src = inputs.audioshare;
+			nativeBuildInputs = with pkgs; [
+				cmake
+				pkg-config
+			];
+			buildInputs = with pkgs; [
+				vcpkg
+			];
+			configurePhase = ''
+				vcpkg install asio protobuf cxxopts spdlog
+			'';
+			buildPhase = ''
+				cmake --preset linux-Release
+				cmake --build --preset linux-Release
+			'';
+			installPhase = ''
+				mkdir -p $out/bin
+				cp out/install/linux-Release/bin/as-cmd $out/bin/${binName}
+			'';
+			env = {
+				VCPKG_TARGET_TRIPLET = "";
+				VCPKG_ROOT = "./server-core";
+			};
+		};
 	};
 }
